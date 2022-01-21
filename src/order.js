822a36732d9d07a62b788c60ec4bea3d0e147bf1
Alexander Bespoyasov <forjob159@gmail.com>
2022-01-21 17:50:05 +0100

    Extract discount selection into a function

--- src/order.js
+++ src/order.js
@@ -4,16 +4,7 @@ function totalPrice(products) {
   return products.reduce((tally, { price, count }) => tally + price * count, 0);
 }
 
-export async function makePurchase({ user, cart, coupon }) {
-  if (!cart.products.length) throw new Error("The cart is empty.");
-  if (user.account < totalPrice(cart.products)) {
-    throw new Error("Not enough money.");
-  }
-
-  const _userId = user.name;
-  const products = cart.products;
-  const total = totalPrice(products);
-
+function selectDiscount(total, coupon) {
   let discount = 0;
   switch (coupon) {
     case "HAPPY_MONDAY":
@@ -25,6 +16,19 @@ export async function makePurchase({ user, cart, coupon }) {
     default:
       discount = 0;
   }
+  return discount;
+}
+
+export async function makePurchase({ user, cart, coupon }) {
+  if (!cart.products.length) throw new Error("The cart is empty.");
+  if (user.account < totalPrice(cart.products)) {
+    throw new Error("Not enough money.");
+  }
+
+  const _userId = user.name;
+  const products = cart.products;
+  const total = totalPrice(products);
+  const discount = selectDiscount(total, coupon);
 
   const order = {
     user: _userId,
